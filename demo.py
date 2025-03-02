38a8be1abc798ca97a0b6e020558204f0016e5ec
samanwaya938 <samanwayaghosh938@gmail.com>
2025-03-02 18:11:46 +0530

    Logging and Exception Configuration Added

---
+++ demo.py
@@ -0,0 +1,28 @@
+# below code is to check the logging configuration
+
+# from src.logger import logging
+
+# logging.info("Logging has started")
+# logging.debug("This is a debug message")
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message")
+
+# --------------------------------------------------------------------------------
+
+# # below code is to check the exception configuration
+
+# from src.exception import MyException
+# from src.logger import logging
+# import sys
+
+# try:
+#     a = 1 / 0
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+    
+   
+
+# --------------------------------------------------------------------------------
+
